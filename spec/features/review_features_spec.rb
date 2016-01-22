c31c42cce17b645e95f83ca21eecc80d160ca760
Katie Smith <klssmith@users.noreply.github.com>
2016-01-22 13:07:32 +0000

    Adds star symbol for average ratings

--- spec/features/review_features_spec.rb
+++ spec/features/review_features_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 require 'web_helper'
 
 feature 'Reviews' do
-  scenario 'displays an average rating for all users' do
-    leave_review('So so', '3')
-    click_link('Sign out')
-    leave_review2('Great', '5')
-    expect(page).to have_content('Average rating: 4')
-  end
-
   scenario 'displays an average rating for all reviews' do
     leave_review('so so', '3')
     click_link('Sign out')
