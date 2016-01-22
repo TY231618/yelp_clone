02fd51d8195939b28d1e8b8768d0abe286a81604
Katie Smith <klssmith@users.noreply.github.com>
2016-01-22 15:37:58 +0000

    Adds database cleaner and endorsements JS

--- spec/features/endorsements_features_spec.rb
+++ spec/features/endorsements_features_spec.rb
@@ -6,10 +6,16 @@ feature 'endorsing reviews' do
     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
+  xscenario 'a user can endorse a review, which updates the review endorsement count' do
     visit '/restaurants'
     click_link 'Endorse Review' #are we endorsing restaurants or the review of the restaurants?
     expect(page).to have_content('1 endorsement')
+  end
+
+  scenario 'a user can endorse a review, which increments the endorsement count', js: true do
+    visit '/restaurants'
+    click_link 'Endorse'
+    expect(page).to have_content("1 endorsement")
   end
 
 end
